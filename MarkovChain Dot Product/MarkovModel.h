e8f6c8c25ce329e4d916b2a3a96ab1d01c5a2653
Ben Wheatley <BenWheatley@users.noreply.github.com>
2024-01-22 22:58:38 +0100

    rough idea of where I'm going with this, but nowhere near "done" (doesn't even work in all the happy paths, and then I need some test cases and some refactoring)

---
+++ MarkovChain Dot Product/MarkovModel.h
@@ -0,0 +1,12 @@
+//
+//  MarkovModel.h
+//  MarkovChain Dot Product
+//
+//  Created by Ben Wheatley on 22/01/2024.
+//
+
+#ifndef MarkovModel_h
+#define MarkovModel_h
+
+
+#endif /* MarkovModel_h */
---
+++ MarkovChain Dot Product/MarkovModel.m
@@ -0,0 +1,8 @@
+//
+//  MarkovModel.m
+//  MarkovChain Dot Product
+//
+//  Created by Ben Wheatley on 22/01/2024.
+//
+
+#import <Foundation/Foundation.h>
